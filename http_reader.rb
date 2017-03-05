ae181e8247a2410a4d23dcbd48a9360ee72d0fd7
Claire Tran <c.tran83@gmail.com>
2017-03-05 17:10:31 +0000

    Updating with the rest of the HTTP section

--- http_reader.rb
+++ http_reader.rb
@@ -1,5 +1,24 @@
+require 'rest-client'
+require 'json'
+
 class HttpReader
   def get(url)
+    response = RestClient.get(url)
+    response
+  end
 
+  def post(url, body)
+    response = RestClient.post(url, body)
+    response
+  end
+
+  def get_status(response)
+    result = JSON.parse(response)
+    puts result['status']
+  end
+
+  def get_country(response)
+    result = JSON.parse(response)
+    result['result']['country']
   end
 end
--- program.rb
+++ program.rb
@@ -1,6 +1,18 @@
 require_relative 'file_reader'
+require_relative 'http_reader'
 
-file_reader = FileReader.new
-file_reader.read_file('addressbook.txt')
-file_reader.find_ages('addressbook.txt')
-file_reader.find_names_ages('addressbook.txt')
+# file_reader = FileReader.new
+# file_reader.read_file('addressbook.txt')
+# file_reader.find_ages('addressbook.txt')
+# file_reader.find_names_ages('addressbook.txt')
+
+http_reader = HttpReader.new
+response = http_reader.get('http://api.postcodes.io/postcodes/tw91ep/nearest')
+puts http_reader.get_status(response)
+
+response = http_reader.get('http://api.postcodes.io/postcodes/tw91ep')
+puts http_reader.get_country(response)
+
+puts 'POST request'
+response = http_reader.post('http://api.postcodes.io/postcodes', {postcodes: ['TW91EP', 'EC1V9JS']})
+puts http_reader.get_status(response)
